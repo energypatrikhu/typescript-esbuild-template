56e7780d2845fabd204b3fd0f358b796256d6b90
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-13 15:16:01 +0200

    Update esbuild.config.mjs to dynamically add external dependencies

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,21 +2,21 @@ import { build } from 'esbuild';
 import { existsSync, rmSync, readFileSync } from 'node:fs';
 
 const __sourceFile = './src/index.ts';
-const __destinationDir = './build';
+const __destinationFile = './build/index.js';
 
-console.log(`Cleaning '${__destinationDir}'...`);
+console.log(`Removing '${__destinationFile}'...`);
 try {
-	if (existsSync(__destinationDir)) {
-		rmSync(__destinationDir, { force: true, recursive: true });
+	if (existsSync(__destinationFile)) {
+		rmSync(__destinationFile, { force: true, recursive: true });
 	}
-	console.log(`Cleaned '${__destinationDir}'`);
+	console.log(`Removed '${__destinationFile}'`);
 } catch (error) {
-	console.error(`Failed to clean '${__destinationDir}'`);
+	console.error(`Failed to remove '${__destinationFile}'`);
 	throw error;
 }
 
 console.log(
-	`\nMinifying '${__sourceFile}' into '${__destinationDir}' directory...`,
+	`\nMinifying '${__sourceFile}' into '${__destinationFile}' file...`,
 );
 try {
 	const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
@@ -24,25 +24,27 @@ try {
 		entryPoints: [__sourceFile],
 		platform: 'node',
 		bundle: true,
-		outdir: __destinationDir,
+		outfile: __destinationFile,
 		logLevel: 'silent',
 		treeShaking: true,
 		minify: true,
 		format: 'cjs',
-		external: [
-			...Object.keys(packageJson.optionalDependencies || {}),
-			...Object.keys(packageJson.bundledDependencies || {}),
-			...Object.keys(packageJson.peerDependencies || {}),
-			...Object.keys(packageJson.devDependencies || {}),
-			...Object.keys(packageJson.dependencies || {}),
-		],
+		external: (() => {
+			const dependencies = new Set();
+			for (const key in packageJson) {
+				if (key.toLowerCase().endsWith('dependencies')) {
+					for (const dep in packageJson[key]) {
+						dependencies.add(dep);
+					}
+				}
+			}
+			return [...dependencies.values()];
+		})(),
 	});
-	console.log(
-		`Minified '${__sourceFile}' into '${__destinationDir}' directory`,
-	);
+	console.log(`Minified '${__sourceFile}' into '${__destinationFile}' file`);
 } catch (error) {
 	console.error(
-		`Failed to minify '${__sourceFile}' into '${__destinationDir}' directory`,
+		`Failed to minify '${__sourceFile}' into '${__destinationFile}' file`,
 	);
 	throw error;
 }
